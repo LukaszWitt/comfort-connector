6dc3a1373363a7868a1b5cd8dbe1dae5c528fa38
Łukasz Witt <witt.config@gmail.com>
2024-10-14 13:02:30 +0200

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Dodaj link do favicon */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,26 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { Box } from '@mui/material';
 import Nav from "@/layout/Nav";
 import Content from "@/layout/Content";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Box className="App" sx={{
-      minHeight: '100svh',
-      minWidth: '100vw',
-      backgroundColor: "#031B31",
-  }}>
-    <Nav/>
-    <Content />
-  </Box>
+    <>
+      <Head>
+        <title>Komfort w zasięgu ręki</title> {/* Tutaj ustawiasz tytuł strony */}
+        <meta name="description" content="Usługi elektryczne, instalacje smart-home, pomiary i certyfikaty energetyczne. Wysoka jakość, mobilność, elastyczność" /> {/* Opcjonalnie, dodaj meta description */}
+      </Head>
+      <Box
+        className="App"
+        sx={{
+          minHeight: '100svh',
+          minWidth: '100vw',
+          backgroundColor: "#031B31",
+        }}
+      >
+        <Nav />
+        <Content />
+      </Box>
+    </>
   );
 }
